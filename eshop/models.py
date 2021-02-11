4d7dcba29bced5d763a8fa13caa4943e5c2d12d9
Akshat Agrawal <akshat7510@gmail.com>
2021-02-11 20:09:14 +0530

    Created product

--- eshop/models.py
+++ eshop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+class Product(models.Model):
+	name = models.CharField(max_length=20)
+	price = models.DecimalField(max_digits=7,decimal_places=2)
+	quantity = models.IntegerField(validators=[MinValueValidator(0)])
+	image = models.ImageField(upload_to=None)
+	description = models.TextField()
+	rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+	def __str__(self):
+		return self.name
