f78aab49b55c8d4bce4a3689f245de447f8e0480
Sahil-Thakur-1499 <sahil.thakur.1499@gmail.com>
2021-02-22 18:15:24 +0530

    Category,ContactUs Pages Added

--- eshop/urls.py
+++ eshop/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup',views.signup,name='signup'),
+	path('category/<str:pk>', views.category_detail, name='category_detail'),
+	path('contact-us',views.contact_us,name='contact_us'),
 ]
--- eshop/views.py
+++ eshop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import *
 from .forms import SignUpForm
 
@@ -19,3 +19,13 @@ def signup(request):
 	else:
 		form = SignUpForm()
 		return render(request, 'eshop/signup.html', {'form': form,'categories':categories})
+
+def category_detail(request,pk):
+	categories=Category.objects.all()
+	ctg = get_object_or_404(Category, name=pk)
+	products = Product.objects.filter(category=ctg.pk)
+	return render(request, 'eshop/category_detail.html', {'products':products,'categories':categories,'ctg':ctg})
+
+def contact_us(request):
+    categories=Category.objects.all()
+    return render(request, 'eshop/contact_us.html', {'categories':categories})
